8dbcff47cc43320bf537472f0d0cf887b6c7cee9
Paolo Roth <iamoctod@gmail.com>
2019-05-06 16:55:27 +0200

    chore: adds ToClass

---
+++ src/decorators/ToClass.ts
@@ -0,0 +1,23 @@
+import { IClass } from '..';
+import StrictTypeError from '../errors/StrictTypeError';
+import { build } from '../helpers/build';
+import canHandleNullOrUndefined from '../helpers/canHandleNullOrUndefined';
+import createToType from '../helpers/createToType';
+import IConfigurationDefaults from '../types/IConfigurationDefaults';
+import IDecoratedEntity from '../types/IDecoratedEntity';
+
+export interface IToClassConfiguration extends IConfigurationDefaults {
+  Type: IClass;
+}
+
+export default createToType<IToClassConfiguration>(arg => {
+  if (arg.config.strict && typeof arg.value !== 'object') {
+    throw new StrictTypeError(arg.value, arg.config.Type);
+  }
+
+  if (canHandleNullOrUndefined(arg)) {
+    return null;
+  }
+
+  return build(arg.config.Type as IDecoratedEntity<any>, arg.value as any);
+});
